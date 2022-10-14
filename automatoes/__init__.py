f1a1a4e0276f8399506e75c01dd1fedd27b7c269
Flavio Garcia <piraz@candango.org>
2022-10-14 13:00:14 -0400

    Delivered 0.9.8 beta 10.

--- automatoes/__init__.py
+++ automatoes/__init__.py
@@ -16,7 +16,7 @@
 # limitations under the License.
 
 __author__ = "Flavio Garcia <piraz@candango.org>"
-__version__ = (0, 9, 8, "b9")
+__version__ = (0, 9, 8, "b10")
 __licence__ = "Apache License V2.0"
 
 
